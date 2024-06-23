33716916de217c890f537ec16a4cc072846eba01
shouta0715 <Myheroes0715@gmail.com>
2024-06-23 22:37:58 +0900

    fix: active device

--- src/features/admin/components/devices/smartphone/index.tsx
+++ src/features/admin/components/devices/smartphone/index.tsx
@@ -1,7 +1,7 @@
-import clsx from "clsx";
 import { useAtomValue } from "jotai";
 import React from "react";
 import { interactionAtom } from "@/features/admin/store";
+import { cn } from "@/lib/utils";
 
 export function SmartPhone({
   children,
@@ -16,7 +16,7 @@ export function SmartPhone({
 
   return (
     <div
-      className={clsx(
+      className={cn(
         "size-full rounded-[40px] border-[12px] bg-white/80 transition-[border-color] duration-300 ease-in-out",
         interactionId === id ? "border-yellow-500" : "border-primary",
         active ? "border-pink-500" : ""
--- src/features/admin/components/devices/tablet/index.tsx
+++ src/features/admin/components/devices/tablet/index.tsx
@@ -1,7 +1,7 @@
-import clsx from "clsx";
 import { useAtomValue } from "jotai";
 import React from "react";
 import { interactionAtom } from "@/features/admin/store";
+import { cn } from "@/lib/utils";
 
 export function Tablet({
   children,
@@ -16,7 +16,7 @@ export function Tablet({
 
   return (
     <div
-      className={clsx(
+      className={cn(
         "relative size-full rounded-[40px] border-[20px] border-black bg-white/80 transition-[border-color] duration-300 ease-in-out",
         interactionId === id ? "border-yellow-500" : "border-primary",
         active ? "border-pink-500" : ""
