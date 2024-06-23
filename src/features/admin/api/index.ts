f3d8cb3315082a02b4f70b0e0e591ec4b1415e32
shouta0715 <Myheroes0715@gmail.com>
2024-06-23 23:35:11 +0900

    add: サイズ変更のapiを追加と型の追加

--- src/features/admin/api/index.ts
+++ src/features/admin/api/index.ts
@@ -22,3 +22,55 @@ export const fetchChangedPosition = async ({
 
   if (!res.ok) throw new Error("Failed to update position");
 };
+
+export async function changeSize({
+  id,
+  appName,
+  width,
+  height,
+}: {
+  id: string;
+  appName: string;
+  width: number;
+  height: number;
+}) {
+  const res = await fetch(`${API_URL}/${appName}/${id}/resize`, {
+    method: "POST",
+    body: JSON.stringify({
+      width,
+      height,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch resize");
+  }
+}
+
+export const changeDeviceData = async ({
+  id,
+  appName,
+  ...data
+}: {
+  id: string;
+  appName: string;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}) => {
+  const res = await fetch(`${API_URL}/${appName}/admin/${id}/device`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch resize");
+  }
+};
