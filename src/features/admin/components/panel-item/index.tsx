bd9d0deb58398c36542986084ea80fe4a6ad5676
shouta0715 <Myheroes0715@gmail.com>
2024-06-23 23:33:02 +0900

    add: panel components

---
+++ src/features/admin/components/panel-item/index.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import clsx from "clsx";
+import { ChevronDown, Loader2 } from "lucide-react";
+import React from "react";
+import {
+  AccordionButton,
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { usePanelItem } from "@/features/admin/hooks/use-panel-item";
+import { UserSession } from "@/features/admin/types";
+
+type PanelItemProps = {
+  session: UserSession;
+  appName: string;
+  active: boolean;
+};
+
+export function PanelItem({ session, appName, active }: PanelItemProps) {
+  const {
+    onClickHandler,
+    onChangePositionHandler,
+    onChangeSizeHandler,
+    action,
+    handleSubmit,
+    isPending,
+    size,
+    position,
+    id,
+  } = usePanelItem({ session, appName, active });
+
+  return (
+    <AccordionItem
+      className="-mx-4 first:-mt-4"
+      onClick={onClickHandler}
+      value={session.id}
+    >
+      <AccordionHeader
+        className={clsx(
+          " flex items-center gap-2 px-4 py-2",
+          active ? "bg-blue-100" : ""
+        )}
+      >
+        <AccordionButton className="shrink-0">
+          <ChevronDown size={16} />
+        </AccordionButton>
+        <form action={action} className="flex-1">
+          <input
+            className="w-full bg-inherit"
+            defaultValue={session.displayname}
+            name="displayname"
+            type="text"
+          />
+        </form>
+      </AccordionHeader>
+      <AccordionContent
+        className={clsx("px-4 py-2", active ? "bg-blue-50" : "")}
+      >
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor={`${id}-width`}>Width</Label>
+            <Input
+              id={`${id}-width`}
+              name="width"
+              onChange={onChangeSizeHandler("width")}
+              type="number"
+              value={size.width}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor={`${id}-height`}>Height</Label>
+            <Input
+              id={`${id}-height`}
+              name="height"
+              onChange={onChangeSizeHandler("height")}
+              type="number"
+              value={size.height}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor={`${id}-x`}>X座標</Label>
+            <Input
+              id={`${id}-x`}
+              name="x"
+              onChange={onChangePositionHandler("x")}
+              type="number"
+              value={position.x}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor={`${id}-y`}>Y座標</Label>
+            <Input
+              id={`${id}-y`}
+              name="y"
+              onChange={onChangePositionHandler("y")}
+              type="number"
+              value={position.y}
+            />
+          </div>
+
+          <Button
+            className="mx-auto w-max px-6 font-semibold"
+            disabled={isPending}
+            size="sm"
+            type="submit"
+          >
+            {isPending && <Loader2 className="mr-2 animate-spin" size={16} />}
+            変更する
+          </Button>
+        </form>
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
