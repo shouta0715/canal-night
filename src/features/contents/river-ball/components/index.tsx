e88ee94bece1b103866092fbad6bbef05914d86f
shouta0715 <Myheroes0715@gmail.com>
2024-06-30 16:56:45 +0900

    add: ボタンを押すとボールが追加される

--- src/features/contents/river-ball/components/index.tsx
+++ src/features/contents/river-ball/components/index.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { useRiverBall } from "@/features/contents/river-ball/hooks/use-river-ball";
 
 function RiverBall() {
-  return <div>index</div>;
+  const { ref, addBallHandler } = useRiverBall();
+
+  return (
+    <div className="relative">
+      <Button
+        className="absolute left-1/2 top-10 -translate-x-1/2"
+        onClick={addBallHandler}
+        variant="outline"
+      >
+        ボールを追加
+      </Button>
+      <div ref={ref} />
+    </div>
+  );
 }
 
 export default RiverBall;
---
+++ src/features/contents/river-ball/hooks/use-river-ball.ts
@@ -0,0 +1,175 @@
+import Matter from "matter-js";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useRiverBall() {
+  const matterEngine = useRef<Matter.Engine | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const render = useRef<Matter.Render | null>(null);
+  const runner = useRef<Matter.Runner | null>(null);
+  const [{ w, h }, setWH] = useState({
+    w: window.innerWidth,
+    h: window.innerHeight,
+  });
+
+  const renderBall = useCallback(
+    (xx: number, yy: number, width: number, wheelSize: number) => {
+      const { Bodies } = Matter;
+
+      const wheelBase = 20;
+      const wheelAOffset = -width * 0.5 + wheelBase;
+
+      const wheelYOffset = 0;
+
+      const ball = Bodies.circle(
+        xx + wheelAOffset,
+        yy + wheelYOffset,
+        wheelSize,
+        {
+          density: 0.0001, // 軽く設定
+          friction: 0.0001, // 転がり摩擦を適度に設定
+          frictionAir: 0.001, // 空気抵抗を適度に設定
+          restitution: 0.6, // 反発係数
+
+          render: {
+            fillStyle: "#ff0000",
+          },
+        }
+      );
+
+      return ball;
+    },
+    []
+  );
+
+  const renderWall = useCallback(
+    (world: Matter.World) => {
+      const { Bodies, Composite } = Matter;
+
+      const left = Bodies.rectangle(0, h / 2, 40, h, {
+        isStatic: true,
+        render: { fillStyle: "#060a19" },
+      });
+      const right = Bodies.rectangle(w, h / 2, 40, h, {
+        isStatic: true,
+        render: { fillStyle: "#060a19" },
+      });
+
+      Composite.add(world, [left, right]);
+    },
+    [h, w]
+  );
+
+  const addBallHandler = useCallback(() => {
+    if (!matterEngine.current) return;
+    const { world } = matterEngine.current;
+
+    const scale = 0.9;
+    const ball = renderBall(150, 100, 150 * scale, 30 * scale);
+
+    Matter.Body.setVelocity(ball, { x: 10, y: -5 });
+
+    Matter.Body.setAngularVelocity(ball, 0.2);
+
+    Matter.Composite.add(world, ball);
+  }, [renderBall]);
+
+  const renderRectangles = useCallback(
+    (world: Matter.World) => {
+      const { Bodies, Composite } = Matter;
+
+      const width = w * 0.7;
+
+      const spacing = Math.max(200, h / 5);
+      const recHeight = 20;
+      const num = Math.floor(h / spacing);
+
+      const rects = Array.from({ length: num }, (_, i) => {
+        const isLast = i === num - 1;
+        const x = i % 2 === 0 ? width / 2 : width;
+        const y = (i + 1) * spacing;
+
+        const angle = Math.PI * 0.07 * (i % 2 === 0 ? 1 : -1);
+
+        return Bodies.rectangle(x, y, isLast ? w : width, recHeight, {
+          isStatic: true,
+          angle: isLast ? 0 : angle,
+          render: { fillStyle: "#060a19" },
+          density: 0.001, // ボールを軽く設定
+          friction: 0.01, // 転がり摩擦を低く設定
+          frictionAir: 0.01, // 空気抵抗を低く設定
+          restitution: 0.6, // 反発係数
+        });
+      });
+
+      Composite.add(world, rects);
+    },
+    [h, w]
+  );
+
+  const onResize = useCallback(() => {
+    setWH({ w: window.innerWidth, h: window.innerHeight });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, [onResize]);
+
+  useEffect(() => {
+    if (!ref.current) return () => {};
+
+    const { Engine, Render, Runner } = Matter;
+
+    matterEngine.current = matterEngine.current || Engine.create();
+    const { world } = matterEngine.current;
+
+    render.current = Render.create({
+      element: document.body,
+      engine: matterEngine.current,
+      options: {
+        width: w,
+        height: h,
+        showAngleIndicator: true,
+        showCollisions: true,
+      },
+    });
+
+    if (!render.current) return () => {};
+
+    Render.run(render.current);
+
+    runner.current = Runner.create();
+
+    Runner.run(runner.current, matterEngine.current);
+
+    renderRectangles(world);
+    renderWall(world);
+
+    return () => {
+      if (render.current) {
+        Render.stop(render.current);
+      }
+
+      if (runner.current) {
+        Runner.stop(runner.current);
+      }
+
+      if (matterEngine.current) {
+        Engine.clear(matterEngine.current);
+      }
+
+      if (render.current) {
+        render.current.canvas.remove();
+      }
+
+      matterEngine.current = null;
+      render.current = null;
+      runner.current = null;
+    };
+  }, [h, renderBall, renderRectangles, renderWall, w]);
+
+  return { ref, addBallHandler };
+}
