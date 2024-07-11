ff0f49ca8fecb3c5ff1890ce41775a8efb1eb82b
shouta0715 <Myheroes0715@gmail.com>
2024-07-11 16:27:17 +0900

    add: UserState Type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,17 @@
 import { Mode } from "@/features/admin/store";
+import { Alignment, AssignedPosition } from "@/features/admin/types";
 
 export type ContentProps = {
   initialMode: Mode;
   id: string;
+};
+
+export type UserState = {
+  width: number;
+  height: number;
+  displayname: string;
+  assignPosition: AssignedPosition;
+  alignment: Alignment;
+  id: string;
+  role: "user";
 };
