75f5bbec666a435c7845153c7e7fb159b9ec6095
shouta0715 <Myheroes0715@gmail.com>
2024-07-11 23:29:03 +0900

    add: ConveyorBeltSushi

---
+++ src/features/contents/conveyor-belt-sushi/components/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import { ConnectMode } from "@/components/connect-mode";
+import { Rail } from "@/features/contents/conveyor-belt-sushi/components/rail";
+import { Sushi } from "@/features/contents/conveyor-belt-sushi/components/sushi";
+import { useConveyorBeltSushi } from "@/features/contents/conveyor-belt-sushi/hooks";
+import { ContentProps } from "@/types";
+
+function ConveyorBeltSushi({ id, initialMode }: ContentProps) {
+  const { isConnecting, mode, sushi, onAnimateComplete, onOverWindow } =
+    useConveyorBeltSushi({
+      initialMode,
+      id,
+    });
+  if (mode === "connect") {
+    return (
+      <ConnectMode
+        appName="conveyor-belt-sushi"
+        id={id}
+        isConnecting={isConnecting}
+      />
+    );
+  }
+
+  return (
+    <div className="flex h-dvh flex-col overflow-hidden">
+      <div className="relative mt-auto">
+        {sushi.map((sushiId) => {
+          return (
+            <Sushi
+              key={sushiId.id}
+              onAnimationComplete={onAnimateComplete}
+              onOverWindow={onOverWindow}
+              sushi={sushiId}
+            />
+          );
+        })}
+        <Rail />
+      </div>
+    </div>
+  );
+}
+
+export default ConveyorBeltSushi;
