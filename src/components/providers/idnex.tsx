99ee538816977688a45298310367f5a826d8ab2e
shouta0715 <Myheroes0715@gmail.com>
2024-07-12 16:11:55 +0900

    fix

--- src/components/providers/idnex.tsx
+++ src/components/providers/idnex.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import { getInitialClientState } from "@/lib/client";
 
@@ -9,9 +8,6 @@ export function ClientProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => getInitialClientState());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
