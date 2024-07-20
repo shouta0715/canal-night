16fa8d399f4e93395fb111bf6eb143bd0c3bb518
shouta0715 <Myheroes0715@gmail.com>
2024-07-20 20:47:03 +0900

    add: リサイズ機能

--- src/features/admin/components/custom/index.tsx
+++ src/features/admin/components/custom/index.tsx
@@ -1,59 +1,68 @@
-import { useSearchParams } from "next/navigation";
 import React from "react";
-import { NodeProps } from "reactflow";
-import { Monitor } from "@/features/admin/components/devices/monitor";
-import { SmartPhone } from "@/features/admin/components/devices/smartphone";
-import { Tablet } from "@/features/admin/components/devices/tablet";
-import { Mode } from "@/features/admin/store";
+import { NodeProps, NodeResizer } from "reactflow";
+
+import { useCustomNode } from "@/features/admin/hooks/use-custom-node";
 import { UserSession } from "@/features/admin/types";
-
-// TODO:割る数を変更する
-const DIVISION_FACTOR = 1;
-
-const division = (value: number | undefined | null): number | null => {
-  if (!value) return null;
-
-  return value / DIVISION_FACTOR;
-};
-
-const devices = {
-  mobile: SmartPhone,
-  tablet: Tablet,
-  monitor: Monitor,
-};
-
-const getDevice = (width: number | undefined | null): keyof typeof devices => {
-  if (!width) return "mobile";
-
-  if (width < 640) return "mobile";
-  if (width < 1024) return "tablet";
-
-  return "monitor";
-};
+import { cn } from "@/lib/utils";
 
 export function CustomSessionNode({ data, id }: NodeProps<UserSession>) {
-  const searchParams = useSearchParams();
-  const node = searchParams.get("node") || "";
-  const mode = (searchParams.get("mode") || "view") as Mode;
-  const { width, height, displayname, alignment } = data;
-
-  const Comp = devices[getDevice(width)];
+  const {
+    inputSize,
+    interactionId,
+    node,
+    setInputSize,
+    onResizeEnd,
+    mode,
+    alignment,
+    displayname,
+  } = useCustomNode({ data, id });
 
   return (
     <div
+      className="relative bg-white/80"
       style={{
-        width: division(width) || 100,
-        height: division(height) || 100,
+        width: inputSize.width,
+        height: inputSize.height,
       }}
     >
-      <Comp active={node === id} id={id} mode={mode}>
+      <NodeResizer
+        handleStyle={{
+          backgroundColor: "black",
+          width: 16,
+          height: 16,
+          zIndex: 9999,
+          borderRadius: 4,
+        }}
+        isVisible={node === id}
+        lineStyle={{
+          borderWidth: 4,
+          borderColor:
+            mode === "connect" ? "rgb(34 197 94)" : "rgb(59 130 246)",
+        }}
+        minHeight={300}
+        minWidth={300}
+        onResize={(_, s) => {
+          setInputSize({
+            width: s.width,
+            height: s.height,
+          });
+        }}
+        onResizeEnd={onResizeEnd}
+      />
+
+      <div
+        className={cn(
+          "size-full p-10",
+          id === node ? "" : "border-4 border-primary",
+          interactionId === id ? "border-4 border-yellow-500" : ""
+        )}
+      >
         <p className="flex h-full items-center justify-center text-4xl font-bold">
           {alignment.isLeft && "👈"}
           {displayname}
-
           {alignment.isRight && "👉"}
         </p>
-      </Comp>
+      </div>
     </div>
   );
 }
