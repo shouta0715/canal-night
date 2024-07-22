add92ca84d37ecb434f12effcfb4406229eb1d4c
shouta0715 <Myheroes0715@gmail.com>
2024-07-22 18:21:25 +0900

    add: provider customs

--- src/features/admin/components/providers/index.tsx
+++ src/features/admin/components/providers/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React, { createContext, useRef } from "react";
+import { CustomsInput } from "@/features/admin/schema";
 import { NodeStore, createNodeStore } from "@/features/admin/store";
 import { UserSession } from "@/features/admin/types";
 
 type RfProviderProps = {
   children: React.ReactNode;
   initialSession?: UserSession[];
+  customs: CustomsInput;
 };
 
 const RFContext = createContext<NodeStore | null>(null);
@@ -14,8 +16,9 @@ const RFContext = createContext<NodeStore | null>(null);
 export function NodeProvider({
   children,
   initialSession = [],
+  customs = [],
 }: RfProviderProps) {
-  const store = useRef(createNodeStore(initialSession)).current;
+  const store = useRef(createNodeStore(initialSession, customs)).current;
 
   return <RFContext.Provider value={store}>{children}</RFContext.Provider>;
 }
