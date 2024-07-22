d0e126f587c13cc351e7d334c4e02122cef65106
shouta0715 <Myheroes0715@gmail.com>
2024-07-22 16:09:37 +0900

    add: custom inputs schema

--- src/features/admin/schema/index.ts
+++ src/features/admin/schema/index.ts
@@ -1,12 +1,80 @@
 import {
   InferInput,
+  array,
   boolean,
+  custom,
   integer,
+  maxLength,
+  minLength,
   minValue,
   number,
   object,
+  picklist,
   pipe,
+  string,
+  trim,
+  variant,
 } from "valibot";
+
+const keySchema = pipe(
+  string("文字列を入力してください。"),
+  trim(),
+  minLength(1, "1文字以上の文字列を入力してください。"),
+  maxLength(30, "30文字以下の文字列を入力してください。"),
+  custom(
+    (value) => {
+      if (typeof value !== "string") return false;
+
+      const isEnglish = /^[a-zA-Z0-9_]+$/.test(value);
+
+      if (!isEnglish) return false;
+      if (value === "width") return false;
+      if (value === "height") return false;
+      if (value === "x") return false;
+      if (value === "y") return false;
+
+      return true;
+    },
+    (v) => {
+      if (typeof v.input !== "string") return "文字列を入力してください。";
+      const isEnglish = /^[a-zA-Z0-9_]+$/.test(v.input);
+
+      if (!isEnglish) return "英数字とアンダースコアのみ使用できます。";
+
+      return `${v.input}は予約語です。`;
+    }
+  )
+);
+
+const labelSchema = pipe(
+  string("ラベルを入力してください。"),
+  trim(),
+  minLength(1, "1文字以上の文字列を入力してください。"),
+  maxLength(30, "30文字以下の文字列を入力してください。")
+);
+
+export const customSchema = variant("type", [
+  object({
+    key: keySchema,
+    type: picklist(["string"]),
+    label: labelSchema,
+    defaultValue: string("初期値の文字列を入力してください。"),
+  }),
+  object({
+    key: keySchema,
+    type: picklist(["number"]),
+    label: labelSchema,
+    defaultValue: number("初期値の数値を入力してください。"),
+  }),
+  object({
+    key: keySchema,
+    type: picklist(["boolean"]),
+    label: labelSchema,
+    defaultValue: boolean(),
+  }),
+]);
+
+export const customsSchema = array(customSchema);
 
 export const deviceSchema = object({
   width: pipe(
@@ -31,3 +99,5 @@ export const deviceSchema = object({
 });
 
 export type DeviceInput = InferInput<typeof deviceSchema>;
+export type CustomInput = InferInput<typeof customSchema>;
+export type CustomsInput = InferInput<typeof customsSchema>;
