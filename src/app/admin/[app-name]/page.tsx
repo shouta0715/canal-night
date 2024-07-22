73f56a67bcc27efb7d29b6ec6383c9c05b20d2dc
shouta0715 <Myheroes0715@gmail.com>
2024-07-22 18:20:22 +0900

    add: getInitialCustoms

--- src/app/admin/[app-name]/page.tsx
+++ src/app/admin/[app-name]/page.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import { API_URL } from "@/constant";
 import { AdminFlow } from "@/features/admin/components";
 import { NodeProvider } from "@/features/admin/components/providers";
+import { CustomsInput } from "@/features/admin/schema";
 import { UserSession } from "@/features/admin/types";
 
 const getInitialSessions = async (appName: string): Promise<UserSession[]> => {
   const res = await fetch(`${API_URL}/${appName}/admin/sessions`, {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  return data;
+};
+
+const getInitialCustoms = async (appName: string): Promise<CustomsInput> => {
+  const res = await fetch(`${API_URL}/${appName}/admin/customs`, {
     cache: "no-store",
   });
 
@@ -20,11 +31,14 @@ export default async function Page({
   params: { "app-name": string };
 }) {
   const appName = params["app-name"];
-  const sessions = await getInitialSessions(appName);
+  const [sessions, customs] = await Promise.all([
+    getInitialSessions(appName),
+    getInitialCustoms(appName),
+  ]);
 
   return (
     <div className="h-dvh w-screen">
-      <NodeProvider initialSession={sessions}>
+      <NodeProvider customs={customs} initialSession={sessions}>
         <AdminFlow />
       </NodeProvider>
     </div>
